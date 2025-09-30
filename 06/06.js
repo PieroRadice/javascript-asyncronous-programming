5bf8e272d31e8aad01603237c98c5c0dfe72761c
PieroRadice <58561490+PieroRadice@users.noreply.github.com>
2025-09-30 16:45:13 +0200

    Update 06.js

--- 06/06.js
+++ 06/06.js
@@ -2,6 +2,7 @@ const getTodos = (callback) => {
   const request = new XMLHttpRequest();
   request.addEventListener("readystatechange", () => {
     if (request.readyState === 4 && request.status === 200) {
+      //JSON.parse(...) È un metodo integrato in JavaScript che trasforma una stringa JSON valida in un oggetto o array JavaScript.
       const data = JSON.parse(request.responseText);
       callback(undefined, data);
     } else if (request.readyState === 4) {
